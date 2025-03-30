9eff0c6091219de1bad80d0832d8c8eb699b1945
Senosiain, Miguel <miguel.senosiain@mmc.com>
2025-03-30 03:13:56 -0300

    Use config service for mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MembersModule } from './members/members.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    MongooseModule.forRoot('mongodb://localhost:27017/parking-control'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI', 'mongodb://localhost:27017/ltrc-parking-control'),
+      }),
+    }),
     MembersModule,
   ],
   controllers: [AppController],
